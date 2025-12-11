354b89b9f73bcb4c05c81bc5e5cfd935ddc0c1a9
A-A-Tel <anouaranthonytel@gmail.com>
2025-12-11 15:03:38 +0100

    expanded types

--- src/types/FieldData.ts
+++ src/types/FieldData.ts
@@ -1,15 +1,63 @@
-export type FieldData = {
+import type {CSSProperties} from "react";
 
-    /**
-     * The internal name of the field
-     */
+export type FieldData =
+    | HiddenField
+    | StringField
+    | NumberField
+    | DateField
+    | TimeField
+    | OptionsField;
+
+type BaseField = {
     fieldName: string;
-
-    /**
-     * The human-readable label for the field.
-     * If omitted, the `fieldName` will be used instead.
-     */
     fieldLabel?: string;
+    required: boolean;
+    value?: string;
+    isKey?: boolean;
+    styles?: CSSProperties | string;
+    regex?: string;
+    placeholder?: string;
+};
 
-    type: string
+export type HiddenField = BaseField & {
+    type: "hidden"
 }
+
+type StringSpecialty = "text" | "tel" | "email" | "password" | "url";
+
+export type StringField = BaseField & {
+    type: "string";
+    minLength: number
+    maxLength: number
+    specialty?: StringSpecialty; // default "text"
+};
+
+export type NumberField = BaseField & {
+    type: "number";
+    min?: number;
+    max?: number;
+    step?: number | string;
+};
+
+type DateSpecialty = "day" | "week" | "month" | "year";
+
+export type DateField = BaseField & {
+    type: "date";
+    specialty?: DateSpecialty; // default "day"
+};
+
+type TimeSpecialty = "hours" | "minutes" | "seconds";
+
+export type TimeField = BaseField & {
+    type: "time";
+    specialty?: TimeSpecialty; // default "minutes"
+};
+
+type OptionsSpecialty = "select" | "radio" | "boxes";
+
+export type OptionsField = BaseField & {
+    type: "options";
+    items: string[];
+    specialty?: OptionsSpecialty; // default "select"
+    allowNewItems: boolean;
+};
--- src/types/FormProps.ts
+++ src/types/FormProps.ts
@@ -1,4 +1,7 @@
+import type {FieldData} from "@lib/types/FieldData.ts";
+
 export interface FormProps {
     title: string;
     route: string;
+    fields: FieldData[]
 }
